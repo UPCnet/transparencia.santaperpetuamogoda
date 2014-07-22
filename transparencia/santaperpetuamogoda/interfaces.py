8767360ebca66827e1a3de015b9bb74e83e95376
Pilar Marinas <pilar.marinas@upcnet.es>
2014-07-22 14:57:04 +0200

    Canvis perque funcioni

--- transparencia/santaperpetuamogoda/interfaces.py
+++ transparencia/santaperpetuamogoda/interfaces.py
@@ -2,4 +2,5 @@ from transparencia.theme.browser.interfaces import ITransparenciaTheme
 
 
 class ISantaPerpetuaLayer(ITransparenciaTheme):
-    """Marker interface for Santa Perpetua de Mogoda Layer depending on the Transparenica one."""
+    """Marker interface for Santa Perpetua de Mogoda Layer
+    depending on the Transparencia one"""
